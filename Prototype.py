ea2bb8aef38b2fbcf63fde85b2a807ac40402670
GilbertTorres6250 <164221646+GilbertTorres6250@users.noreply.github.com>
2025-12-11 00:07:10 -0700

    12/10 Prototype.py
    Fixed the issue with functions not being able to used on self by using another parameter. Now it looks for user and target. as well as now the when the user makes a move they have to pick the number from their deck.
    
    Also now starting on getting all the numbers to be moves in CLI like for the shop.
    
    ADDED shield, shield resets after opponents turn.
    
    NEED to add print that displays cards, deck number for cards, and i want to add levels to cards so ill make them also display card level. Card levels will come from buying duplicates in store.

--- Prototype.py
+++ Prototype.py
@@ -1,3 +1,4 @@
+from operator import index
 import FindableFunctions
 from random import randint
 from random import randrange
@@ -5,50 +6,56 @@ from colorama import Fore, Back, Style, init
 init()
 player={
     "health":100,
+    "shield":0,
     "deck":[],
 }
-openent={
+oppenent={
     "health":100,
-    "deck":[randint(1,2),randint(3,4)]
+    "shield":0,
+    "deck":[randint(1,4)]
 }
 
 game=True
 turn=0
-offers=["attack",2,3,4]
+offers=["attack","block","heal","heavy"]
 print(Fore.YELLOW+"❤❤❤SHOP TIME❤❤❤")
 print(f"{offers}"+Style.RESET_ALL)
 player_input=int(input("pick one "))-1
-player["deck"].append(offers[player_input])
+player["deck"].append(player_input + 1)
 
-print(Fore.RED+f"your oppenent picks {openent["deck"]}"+Fore.RESET)
+print(Fore.RED+f"your oppenent picks {oppenent["deck"]}"+Fore.RESET)
 
 while game:
     turn+=1
-    opp=openent["deck"][randrange(0,len(openent["deck"]))]
+    opp=oppenent["deck"][randrange(0,len(oppenent["deck"]))]
     if turn%2==1:#PLAYER PLAYER PLAYER PLAYER PLAYER PLAYER PLAYER PLAYER PLAYER PLAYER PLAYER PLAYER PLAYER
-        print(Fore.RESET+f"Current Deck:{player["deck"]}")
+        print(Fore.BLUE+f"Current Deck:{player["deck"]}")
         print(f"Current health:{player["health"]}")
         move = int(input("what to play "))
-        func = FindableFunctions.convert(move)
+        func = FindableFunctions.convert(int(index(player["deck"][move-1])))
         if func:
-            func(openent)
+            if 0 <= move - 1 < len(player["deck"]):
+                func(player, oppenent)
+            else:
+                print("Card not in deck")
         else:
             print("invalid move")
+        oppenent["shield"]=0
 
 
     else: #NPC NPC NPC NPC NPC NPC NPC NPC NPC NPC NPC NPC NPC NPC NPC NPC NPC NPC NPC NPC NPC NPC
-        print(Fore.RED+f"oppenent health: {openent["health"]}")
+        print(Fore.RED+f"oppenent health: {oppenent["health"]}")
         NPC_func=FindableFunctions.convert(opp)
-        NPC_func(player)
-        print(f"oppenent plays {NPC_func}")
+        NPC_func(oppenent,player)
+        print(f"oppenent plays {NPC_func.__name__}")
+        player["shield"]=0
 
 
     if turn%8==0: #SHOP SHOP SHOP SHOP SHOP SHOP SHOP SHOP SHOP SHOP SHOP SHOP SHOP SHOP SHOP
         print(Fore.YELLOW+"❤❤❤SHOP TIME❤❤❤")
-        shop=[randint(1,4),randint(1,4)]
+        shop= offers[randint(0,3)]
+
         print(shop)
         player_input = int(input("pick one ")) - 1
-        player["deck"].append(offers[player_input])
+        player["deck"].append(player_input + 1)
     print(f"******turn:{turn}******")
-
-
