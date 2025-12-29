44f2cc8d8fb74746bffd47e6e66c5edc06de11f1
GilbertTorres6250 <164221646+GilbertTorres6250@users.noreply.github.com>
2025-12-29 13:16:53 -0700

    12/29 GameClasses.py
    ADDED
    - ENEMY. check living() to remove the dead enemies from the field once they die
    -ENEMY. play card() so enemies can play cards from their deck

--- GameClasses.py
+++ GameClasses.py
@@ -2,6 +2,7 @@ import GameFunctions as GF
 from random import randint
 from random import randrange
 from colorama import Fore, Style, init
+from operator import index
 
 class Character:
 	id = []
@@ -34,6 +35,7 @@ class Character:
 		name = self.__class__.__name__
 		print(color+f"[{name}:{full * current_health}{empty * missing_health}]")
 
+
 class Player(Character):
 	def __init__(self):
 		super().__init__("player_group", 10)
@@ -41,9 +43,23 @@ class Player(Character):
 class Enemy(Character):
 	enemy_id = []
 
-	def __init__(self, health=100):
+	def __init__(self, health=10):
 		super().__init__("enemy_group", health)
 		Enemy.enemy_id.append(self)
+	def check_living(self):
+		if self.health <= 0:
+			Enemy.enemy_id.remove(self)
+			return False
+		return True
+
+	def play_card(self, target):
+		if not self.deck:
+			return
+		card = self.deck[randrange(len(self.deck))]
+		func = GF.convert(card)
+		level = GF.get_card_level(self.deck, card)
+		func(self, target, level)
+
 
 class Bee(Enemy):
 	def __init__(self):
@@ -70,7 +86,7 @@ class Shop:
 
 	def open_shop(self):
 		Shop.current_shop.clear()
-		for _ in range(self.size):
+		for item in range(self.size):
 			Shop.current_shop.append(Shop.offers[randrange(0, 5)])
 		print(Fore.YELLOW + "$$$SHOP TIME$$$")
 		print(Shop.current_shop)#Prints shop
@@ -78,8 +94,8 @@ class Shop:
 		if 0 <= player_choice < len(Shop.current_shop):
 			picked_move = Shop.current_shop[player_choice]
 			picked_move= Shop.offers.index(picked_move)
-			print(f"YOU JUST BOUGHT {GF.convert(picked_move+1).__name__.upper()}"+ Fore.RESET)
+			print(f"YOU JUST BOUGHT {GF.convert(picked_move+1).__name__.upper()}")
 			self.user.add_card(picked_move+1)
 		else:
-			print("WE DONT HAVE THAT IN STOCK"+ Fore.RESET)
+			print("WE DONT HAVE THAT IN STOCK")
 		self.user.convert_hand()
