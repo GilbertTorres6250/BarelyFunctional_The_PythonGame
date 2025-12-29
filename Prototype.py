6a8fd6feb4bd807515da1a7b97ea3cfa1e3cab9c
GilbertTorres6250 <164221646+GilbertTorres6250@users.noreply.github.com>
2025-12-29 13:32:06 -0700

    12/29 Prototype.py
    ADDED
    - enemy logic for attacks and shop
    ALSO
    -renamed some things for clarity and consistency

--- Prototype.py
+++ Prototype.py
@@ -3,6 +3,7 @@ import GameClasses as GC
 from random import randint
 from random import randrange
 from operator import index
+from colorama import Fore
 
 
 human = GC.Player()
@@ -10,6 +11,8 @@ dan = GC.Bee()
 dave = GC.Bee()
 turn = 0
 shop = GC.Shop(5,human).open_shop()
+for enemyEntity in GC.Enemy.enemy_id:
+	enemyEntity.add_card(randrange(1, 5))
 game=True
 
 while game:
@@ -20,27 +23,44 @@ while game:
 		human.display_health()
 		human.show_hand()
 		enemy_index=0
-		for x in GC.Enemy.enemy_id:
+		for enemyEntity in GC.Enemy.enemy_id:
 			enemy_index+=1
-			print(enemy_index,x.__class__.__name__,"| HEALTH:", x.health)
+			print(enemy_index,enemyEntity.__class__.__name__,"| HEALTH:", enemyEntity.health)
 		target = int(input("WHICH ENEMY?(1,2,3,4): "))
 		monster=GC.Enemy.enemy_id[target-1]
 		move = int(input("WHAT MOVE?(1,2,3,4): "))
 		try:
-			card_id = index(list(dict.fromkeys(human.deck))[move - 1])  # ?this is the move in the function map
+			card_id = index(list(dict.fromkeys(human.deck))[move - 1])  #? this is the move in the function map
 		except LookupError:
-			card_id = index(list(dict.fromkeys(human.deck))[0])  # ?this is the move in the function map
-		func = GF.convert(card_id)  # ?this is the function out of the function map being preformed
-		level = GF.get_card_level(human.deck, card_id)  # ? number of dupes
-		if 0 <= move - 1 < len(human.deck):  # ?ensures var(move) is in the range of the hand
-			func(human, monster, level)  # ?passes user, target, and level
+			card_id = index(list(dict.fromkeys(human.deck))[0])  #? this is the move in the function map
+		func = GF.convert(card_id)  #? this is the function out of the function map being preformed
+		level = GF.get_card_level(human.deck, card_id)  #? number of dupes
+		if 0 <= move - 1 < len(human.deck):  #? ensures var(move) is in the range of the hand
+			func(human, monster, level)  #? passes user, target, and level
 		else:
 			print("MOVE NOT IN DECK")
-		for x in GC.Enemy.enemy_id:
-			x.shield=0
+		for enemyEntity in GC.Enemy.enemy_id:
+			enemyEntity.shield=0
+			enemyEntity.check_living()
 	else:
-		dan.display_health()
-		dave.display_health()
+		for enemyEntity in GC.Enemy.enemy_id:
+			enemyEntity.display_health()
+			enemyEntity.convert_hand()
+			enemyEntity.show_hand()
+			enemyEntity.play_card(human)
+
+			enemyEntity.hand = []
 
 	if turn%3==0:
 		shop = GC.Shop(5,human).open_shop()
+		for enemyEntity in GC.Enemy.enemy_id:
+			enemyEntity.add_card(randrange(1, 5))
+
+	print(f"************TURN:{turn}************")
+	if human.health <= 0:
+		print(Fore.RESET+f"YOU DIED\nTurn {turn}")
+		game= False
+	if len(GC.Enemy.enemy_id) == 0:
+		print(Fore.RESET+f"YOU WON\nTurn {turn}")
+		game = False
+
